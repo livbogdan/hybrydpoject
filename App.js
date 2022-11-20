9b507bd3a5b5025f2715f35670d1f8fba1240e5e
livbogdan <livbogdan@gmail.com>
2022-11-20 18:31:33 +0100

    first uploaad

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 
-export default function App() {
+export default function App() 
+{
+  let age = 30;
+  let exampleFunction = () => 
+  {
+    console.log("saved function 3");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ width: "100%", height: 200, backgroundColor: "green" }}>
+        <View style={{ width: "20%", height: "20%", backgroundColor: "yellow" }} />
+      </View>
+      <Text>Hello There!</Text>
+      <Text style={{ color: "red", fontWeight: "bold" }}>My age is {age}</Text>
+      <Button
+        title={"Press me to use a function stored in a variable"}
+        onPress={exampleFunction}
+      />
+      <Button
+        title={"Press me to run an arrow function"}
+        onPress={() => {
+          console.log("Hello there");
+        }}
+      />
+      <Button
+        title={"Example of WRONG way (don't **call** the function inline)"}
+        onPress={exampleFunction()}
+      />
+      <TextInput
+        placeholder="Enter something here"
+        onChangeText={(newText) => {
+          console.log(newText);
+        }}
+      ></TextInput>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
