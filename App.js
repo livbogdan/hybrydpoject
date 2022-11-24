7618fdbc43902526831e8a6a8cdca323b9678420
livbogdan <livbogdan@gmail.com>
2022-11-24 11:06:58 +0100

    Add any plugins

--- App.js
+++ App.js
@@ -43,8 +43,9 @@ const styles = StyleSheet.create({
   container: 
   {
     flex: 1,
-    backgroundColor: "fff",
+    backgroundColor: "black",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "top",
+    
   },
 });
