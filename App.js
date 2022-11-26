47b9ba95e7a8e80269090fdf73eb58a512989efd
livbogdan <livbogdan@gmail.com>
2022-11-26 03:45:57 +0100

    ScratchUpdate

--- App.js
+++ App.js
@@ -3,49 +3,29 @@ import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 
 export default function App() 
 {
-  let age = 30;
-  let exampleFunction = () => 
-  {
-    console.log("saved function 3");
-  };
-  return (
-    <View style={styles.container}>
-      <View style={{ width: "100%", height: 200, backgroundColor: "green" }}>
-        <View style={{ width: "20%", height: "20%", backgroundColor: "yellow" }} />
+
+  return 
+  (
+    <View style = {styles.appContainer}>
+      <View style = {styles.inputContainer}>
+        <TextInput placeholder="Your Goal!!!"/>
+        <Button title="Add Goal"/>
       </View>
-      <Text>Hello There!</Text>
-      <Text style={{ color: "red", fontWeight: "bold" }}>My age is {age}</Text>
-      <Button
-        title={"Press me to use a function stored in a variable"}
-        onPress={exampleFunction}
-      />
-      <Button
-        title={"Press me to run an arrow function"}
-        onPress={() => {
-          console.log("Hello there");
-        }}
-      />
-      <Button
-        title={"Example of WRONG way (don't **call** the function inline)"}
-        onPress={exampleFunction()}
-      />
-      <TextInput
-        placeholder="Enter something here"
-        onChangeText={(newText) => {
-          console.log(newText);
-        }}
-      ></TextInput>
+      <View>
+        <Text>List Of Goal</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: 
+const styles = StyleSheet.create(
   {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "top",
-    
+  appContainer: 
+  {
+    padding: 50,
   },
+  inputContainer:
+  {
+    flexDirection: "row"
+  }
 });
